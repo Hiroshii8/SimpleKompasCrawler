c86d13e3954986c63e17ac872cab0654107efe97
Hiroshii8 <36793851+Hiroshii8@users.noreply.github.com>
2019-07-24 10:39:48 +0700

    Fix Typo

--- kompas_crawler.py
+++ kompas_crawler.py
@@ -2,17 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-def kompas_crawl(url):
+def SimpleCrawler(url):
 	result = []
 	req = requests.get(url)
 	soup = BeautifulSoup(req.text, "lxml")
 
-	#find paging page
+	#Iterate Every Page Until End
 	paging = soup.find_all("div", {'class' : 'paging clearfix'})
 	paging_link = paging[0].find_all('a',{'class' : 'paging__link'})
 	last_page = int([item.get('href').split('/')[-1] for item in paging_link][-1])
 	print(paging)
-	#looping through link
+	
 	for i in range(1, last_page):
 		print(url+str(i))
 
@@ -21,31 +21,30 @@ def kompas_crawl(url):
 		soup = BeautifulSoup(req.text, "lxml")
 		new_links = soup.find_all("div",{'class':'article__list clearfix'})
 
-		#looping through article link
+		
 		for idx,news in enumerate(new_links):
 			news_dict = {}
 
-			#find news title
+			#News Title
 			title_news = news.find('a',
 				{'class':'article__link'}).text
-			#find url news
+			
+			#News URL
 			url_news = news.find('a',
 				{'class':'article__link'}).get('href')
 
-			#find news content in url
+			#News Link
 			req_news = requests.get(url_news)
 			soup_news = BeautifulSoup(req_news.text, "lxml")
 
-			#find news content
+			#News Content
 			news_content = soup_news.find("div",
 				{'class':'read__content'})
-
-			#find paragraph in news content
 			p = news_content.find_all('p')
 			content = ' '.join(item .text for item in p)
 			news_content = content.encode('utf8','replace')
 
-			#wrap in dictionary
+			#Indexing
 			news_dict['id'] = idx
 			news_dict['url'] = url_news
 			news_dict['title'] = title_news
@@ -53,7 +52,6 @@ def kompas_crawl(url):
 			result.append(news_dict)
 	return result
 
-url = 'http://indeks.kompas.com/news/2017-08-04/'
-crawl  = kompas_crawl(url)
-with open("kompas.json","w") as f:
-    json.dump(crawl,f)
+Url = 'URL_KOMPAS'
+res = SimpleCrawler(Url)
+print(res)
